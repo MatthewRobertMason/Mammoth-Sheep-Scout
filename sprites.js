c71215affcf61ca293730da74698cf7acfb88507
Adam Douglass <douglass@malloc.ca>
2018-04-22 00:51:08 -0400

    Load sprites in advance

--- sprites.js
+++ sprites.js
@@ -1,7 +1,17 @@
 
 
 var Sprites = new Map([
-    ["Graphics/AirDropParachute.png", null]
+    ["Graphics/AirDropParachute.png", null],
+    ["Graphics/GunBase.png", null],
+    ["Graphics/GunTurret.png", null],
+    ["Graphics/Frame.png", null],
+    ["Graphics/City.png", null],
+    ["Graphics/Rocket.png", null],
+    ["Graphics/Fireball.png", null],
+    ["Graphics/AirDropCrate.png", null],
+    ["Graphics/City_1.png", null],
+    ["Graphics/City_2.png", null],
+    ["Graphics/City_Fallen.png", null],
 ])
 
 
